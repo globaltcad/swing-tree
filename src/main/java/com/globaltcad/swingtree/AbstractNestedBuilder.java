6135ba7615cb64a053ca3dc334cc4b4d78cd2f6c
dnepp <d.nepp@globaltcad.com>
2022-05-02 17:50:25 +0200

    try fix generic args

--- src/main/java/com/globaltcad/swingtree/AbstractNestedBuilder.java
+++ src/main/java/com/globaltcad/swingtree/AbstractNestedBuilder.java
@@ -93,8 +93,9 @@ abstract class AbstractNestedBuilder<I, C extends E, E> extends AbstractBuilder<
      * @param builders An array of builder instances whose JComponents ought to be added to the one wrapped by this builder.
      * @return This very instance, which enables builder-style method chaining.
      */
+    @SafeVarargs
     @SuppressWarnings("varargs")
-    public final I add(AbstractNestedBuilder<?, ?, ?>... builders) {
+    public final <B extends AbstractNestedBuilder<?, ?, ?>> I add(B... builders) {
         if ( builders == null )
             throw new IllegalArgumentException("Swing tree builders may not be null!");
 
