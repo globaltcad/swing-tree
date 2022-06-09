cc0a24fab4c032ead55dc0dce11b438f40b095f3
Gleethos <danneydan@gmx.at>
2022-06-09 08:06:44 +0200

    improve documentation

--- src/main/java/com/globaltcad/swingtree/UIForLabel.java
+++ src/main/java/com/globaltcad/swingtree/UIForLabel.java
@@ -28,6 +28,12 @@ public class UIForLabel<L extends JLabel> extends UIForAbstractSwing<UIForLabel<
         return this;
     }
 
+    /**
+     *  Use this to transform the underlying {@link JLabel} into a clickable link.
+     *
+     * @param href A string containing a valid URL used as link hyper reference.
+     * @return This very builder to allow for method chaining.
+     */
     public UIForLabel<L> makeLinkTo(String href) {
         LazyRef<String> text = LazyRef.of(_component::getText);
         if ( !href.startsWith("http") ) href = "https://" + href;
@@ -77,26 +83,77 @@ public class UIForLabel<L extends JLabel> extends UIForAbstractSwing<UIForLabel<
         return this;
     }
 
+    /**
+     *  A convenience method to avoid peeking into this builder like so:
+     *  <pre>{@code
+     *     UI.label("Something")
+     *         .peek( label -> label.setHorizontalAlignment(...) );
+     *  }</pre>
+     *
+     * @param horizontalAlign The horizontal alignment which should be applied to the underlying component.
+     * @return This very builder to allow for method chaining.
+     */
     public UIForLabel<L> withPosition(UI.HorizontalAlign horizontalAlign) {
         _component.setHorizontalAlignment(horizontalAlign.forSwing());
         return this;
     }
 
+    /**
+     *  A convenience method to avoid peeking into this builder like so:
+     *  <pre>{@code
+     *     UI.label("Something")
+     *         .peek( label -> label.setHorizontalTextPosition(...) );
+     *  }</pre>
+     *
+     * @param horizontalAlign The horizontal alignment which should be applied to the text of the underlying component.
+     * @return This very builder to allow for method chaining.
+     */
     public UIForLabel<L> withTextPosition(UI.HorizontalAlign horizontalAlign) {
         _component.setHorizontalTextPosition(horizontalAlign.forSwing());
         return this;
     }
 
-    public UIForLabel<L> withPosition(UI.VerticalAlign horizontalAlign) {
-        _component.setVerticalAlignment(horizontalAlign.forSwing());
+    /**
+     *  A convenience method to avoid peeking into this builder like so:
+     *  <pre>{@code
+     *     UI.label("Something")
+     *         .peek( label -> label.setVerticalAlignment(...) );
+     *  }</pre>
+     *
+     * @param verticalAlign The vertical alignment which should be applied to the underlying component.
+     * @return This very builder to allow for method chaining.
+     */
+    public UIForLabel<L> withPosition(UI.VerticalAlign verticalAlign) {
+        _component.setVerticalAlignment(verticalAlign.forSwing());
         return this;
     }
 
-    public UIForLabel<L> withTextPosition(UI.VerticalAlign horizontalAlign) {
-        _component.setVerticalTextPosition(horizontalAlign.forSwing());
+    /**
+     *  A convenience method to avoid peeking into this builder like so:
+     *  <pre>{@code
+     *     UI.label("Something")
+     *         .peek( label -> label.setVerticalTextPosition(...) );
+     *  }</pre>
+     *
+     * @param verticalAlign The vertical alignment which should be applied to the text of the underlying component.
+     * @return This very builder to allow for method chaining.
+     */
+    public UIForLabel<L> withTextPosition(UI.VerticalAlign verticalAlign) {
+        _component.setVerticalTextPosition(verticalAlign.forSwing());
         return this;
     }
 
+    /**
+     *  A convenience method to avoid peeking into this builder like so:
+     *  <pre>{@code
+     *     UI.label("Something")
+     *         .peek( label -> label.setIcon(...) );
+     *  }</pre>
+     *
+     *
+     * @param icon The {@link Icon} which should be displayed on the label.
+     * @return This very builder to allow for method chaining.
+     */
     public UIForLabel<L> withIcon(Icon icon) {
         LogUtil.nullArgCheck(icon,"icon",Icon.class);
         _component.setIcon(icon);
