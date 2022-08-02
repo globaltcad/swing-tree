1f1e517cb072fe8b64d59e58a8e4f0c20e716b2b
dnepp <d.nepp@globaltcad.com>
2022-08-02 14:21:14 +0200

    improve tabbed pane event registration support

--- src/main/java/com/globaltcad/swingtree/Tab.java
+++ src/main/java/com/globaltcad/swingtree/Tab.java
@@ -1,6 +1,9 @@
 package com.globaltcad.swingtree;
 
+import com.globaltcad.swingtree.api.UIAction;
+
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
 
 /**
  *  An immutable data carrier exposing everything needed to configure a tab of a {@link JTabbedPane}.
@@ -20,37 +23,50 @@ public class Tab
     private final String _title;
     private final Icon _icon;
     private final String _tip;
+    private final UIAction<SimpleDelegate<JTabbedPane, ChangeEvent>> _onSelected;
 
-    Tab(JComponent contents, String title, Icon icon, String tip) {
+    Tab(
+            JComponent contents,
+            String title,
+            Icon icon,
+            String tip,
+            UIAction<SimpleDelegate<JTabbedPane, ChangeEvent>> onSelected
+    ) {
         LogUtil.nullArgCheck(title,"title",String.class);
         _contents = contents;
         _title = title;
         _icon = icon;
         _tip = tip;
+        _onSelected = onSelected;
     }
 
     public final Tab withIcon(Icon icon) {
         LogUtil.nullArgCheck(icon,"icon",Icon.class);
         if ( _contents != null ) throw new IllegalArgumentException("Tab object may not be called anymore after the contents were specified!");
         if ( _icon != null ) throw new IllegalArgumentException("Icon already specified!");
-        return new Tab(_contents, _title, icon, _tip);
+        return new Tab(_contents, _title, icon, _tip, _onSelected);
     }
 
     public final Tab withTip(String tip) {
         LogUtil.nullArgCheck(tip,"tip",String.class);
         if ( _contents != null ) throw new IllegalArgumentException("Tab object may not be called anymore after the contents were specified!");
         if ( _tip != null ) throw new IllegalArgumentException("Tip already specified!");
-        return new Tab(_contents, _title, _icon, tip);
+        return new Tab(_contents, _title, _icon, tip, _onSelected);
     }
 
     public final Tab add(UIForAbstractSwing<?,?> contents) {
         if ( _contents != null ) throw new IllegalArgumentException("Contents already specified!");
-        return new Tab(contents.getComponent(), _title, _icon, _tip);
+        return new Tab(contents.getComponent(), _title, _icon, _tip, _onSelected);
     }
 
     public final Tab add(JComponent contents) {
         if ( _contents != null ) throw new IllegalArgumentException("Contents already specified!");
-        return new Tab(contents, _title, _icon, _tip);
+        return new Tab(contents, _title, _icon, _tip, _onSelected);
+    }
+
+    public final Tab on(UIAction<SimpleDelegate<JTabbedPane, ChangeEvent>> onSelected) {
+        if ( _onSelected != null ) throw new IllegalArgumentException("Selection event already specified!");
+        return new Tab(_contents, _title, _icon, _tip, onSelected);
     }
 
     public JComponent contents() { return _contents; }
@@ -60,4 +76,8 @@ public class Tab
     public Icon icon() { return _icon; }
 
     public String tip() { return _tip; }
+
+    public UIAction<SimpleDelegate<JTabbedPane, ChangeEvent>> onSelection() {
+        return _onSelected;
+    }
 }
--- src/main/java/com/globaltcad/swingtree/UIForTabbedPane.java
+++ src/main/java/com/globaltcad/swingtree/UIForTabbedPane.java
@@ -1,6 +1,12 @@
 package com.globaltcad.swingtree;
 
+import com.globaltcad.swingtree.api.UIAction;
+
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 /**
  *  A swing tree builder node for {@link JTabbedPane} instances.
@@ -16,7 +22,20 @@ public class UIForTabbedPane<P extends JTabbedPane> extends UIForAbstractSwing<U
     public UIForTabbedPane(P component) { super(component); }
 
     public final UIForTabbedPane<P> add(Tab tab) {
+        if ( tab.onSelection() != null ) {
+            int index = _component.getTabCount();
+            _component.addChangeListener(e -> {
+                if ( index == _component.getSelectedIndex() )
+                    tab.onSelection().accept(new SimpleDelegate<>(_component, e, ()->getSiblinghood()));
+            });
+        }
         _component.addTab(tab.title(), tab.icon(), tab.contents(), tab.tip());
+        return this;
+    }
+
+    public final UIForTabbedPane<P> onChange(UIAction<SimpleDelegate<P, ChangeEvent>> onChange) {
+        LogUtil.nullArgCheck(onChange, "onChange", UIAction.class);
+        _component.addChangeListener(e -> onChange.accept(new SimpleDelegate<>(_component, e, ()->getSiblinghood())));
         return this;
     }
 
