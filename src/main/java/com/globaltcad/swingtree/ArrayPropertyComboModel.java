0f9494b950fe55e4b58797b4c844618bc0eeff26
dnepp <d.nepp@globaltcad.com>
2022-11-29 14:54:10 +0100

    add combo box factory method allowing for immutable items property

--- src/main/java/com/globaltcad/swingtree/ArrayPropertyComboModel.java
+++ src/main/java/com/globaltcad/swingtree/ArrayPropertyComboModel.java
@@ -1,26 +1,40 @@
 package com.globaltcad.swingtree;
 
+import com.globaltcad.swingtree.api.mvvm.Val;
 import com.globaltcad.swingtree.api.mvvm.Var;
 
 class ArrayPropertyComboModel<E> extends AbstractComboModel<E>
 {
-	private final Var<E[]> items;
+	private final Val<E[]> _items;
+	private final boolean _mutable;
 
-	ArrayPropertyComboModel(Var<E> selection, Var<E[]> items) {
+	ArrayPropertyComboModel( Var<E> selection, Val<E[]> items ) {
 		super(selection);
-		this.items = items;
-		this._selectedIndex = _indexOf(_selectedItem.orElseNull());
+		_items = items;
+		_selectedIndex = _indexOf(_selectedItem.orElseNull());
+		_mutable = false;
+	}
+
+	ArrayPropertyComboModel( Var<E> selection, Var<E[]> items ) {
+		super(selection);
+		_items = items;
+		_selectedIndex = _indexOf(_selectedItem.orElseNull());
+		_mutable = true;
 	}
 
 	@Override
-	public AbstractComboModel<E> withVar(Var<E> newVar) {
-		return new ArrayPropertyComboModel<>(newVar, items);
+	public AbstractComboModel<E> withVar( Var<E> newVar ) {
+		return new ArrayPropertyComboModel<>(newVar, _items);
 	}
 
 	@Override protected void setAt(int index, E element) {
-		items.ifPresent( i -> { i[index] = element; items.act(); });
+		if ( _mutable )
+			_items.ifPresent(i -> {
+				i[index] = element;
+				if ( _items instanceof Var ) ((Var<E>) _items).act();
+			});
 	}
-	@Override public int getSize() { return items.map( i -> i.length ).orElse(0); }
-	@Override public E getElementAt( int index ) { return items.map( i -> i[index] ).orElseNull(); }
+	@Override public int getSize() { return _items.map(i -> i.length ).orElse(0); }
+	@Override public E getElementAt( int index ) { return _items.map(i -> i[index] ).orElseNull(); }
 
 }
