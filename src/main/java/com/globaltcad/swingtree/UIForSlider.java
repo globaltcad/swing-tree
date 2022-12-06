7464ec0620459d4461bc0f9a7704a6392331eae6
Gleethos <danneydan@gmx.at>
2022-12-06 08:12:45 +0100

    improve documentation

--- src/main/java/com/globaltcad/swingtree/UIForSlider.java
+++ src/main/java/com/globaltcad/swingtree/UIForSlider.java
@@ -39,6 +39,13 @@ public class UIForSlider<S extends JSlider> extends UIForAbstractSwing<UIForSlid
         getComponent().addChangeListener(action::accept);
     }
 
+    /**
+     * Sets the minimum value of the slider.
+     * For more information see {@link JSlider#setMinimum(int)}.
+     *
+     * @param min The minimum value of the slider.
+     * @return This very instance, which enables builder-style method chaining.
+     */
     public final UIForSlider<S> withMin( int min ) {
         getComponent().setMinimum( min );
         return this;
@@ -51,10 +58,17 @@ public class UIForSlider<S extends JSlider> extends UIForAbstractSwing<UIForSlid
      */
     public final UIForSlider<S> withMin( Val<Integer> min ) {
         NullUtil.nullArgCheck( min, "min", Val.class );
-        _onShow(min, v -> getComponent().setMinimum(v) );
+        _onShow( min, v -> getComponent().setMinimum(v) );
         return this;
     }
 
+    /**
+     * Sets the maximum value of the slider.
+     * For more information see {@link JSlider#setMaximum(int)} (int)}.
+     *
+     * @param max The maximum value of the slider.
+     * @return This very instance, which enables builder-style method chaining.
+     */
     public final UIForSlider<S> withMax( int max ) {
         getComponent().setMaximum( max );
         return this;
@@ -67,25 +81,80 @@ public class UIForSlider<S extends JSlider> extends UIForAbstractSwing<UIForSlid
      */
     public final UIForSlider<S> withMax( Val<Integer> max ) {
         NullUtil.nullArgCheck( max, "max", Val.class );
-        _onShow(max, v -> getComponent().setMaximum(v) );
+        _onShow( max, v -> getComponent().setMaximum(v) );
         return this;
     }
 
+    /**
+     * Sets the current value of the slider.
+     * For more information see {@link JSlider#setValue(int)}.
+     *
+     * @param value The current value of the slider.
+     * @return This very instance, which enables builder-style method chaining.
+     */
     public final UIForSlider<S> withValue( int value ) {
         getComponent().setValue( value );
         return this;
     }
 
+    /**
+     * @param val An integer property used to dynamically update the value of the slider.
+     * @return This very instance, which enables builder-style method chaining.
+     * @throws IllegalArgumentException if {@code value} is {@code null}.
+     */
     public final UIForSlider<S> withValue( Val<Integer> val ) {
         NullUtil.nullArgCheck( val, "val", Val.class );
-        _onShow(val, v -> getComponent().setValue(v) );
+        _onShow( val, v -> getComponent().setValue(v) );
         return this;
     }
 
+    /**
+     * @param var An integer property used to dynamically update the value of the slider.
+     * @return This very instance, which enables builder-style method chaining.
+     * @throws IllegalArgumentException if {@code value} is {@code null}.
+     */
     public final UIForSlider<S> withValue( Var<Integer> var ) {
         NullUtil.nullArgCheck( var, "var", Var.class );
         _onChange( e -> _doApp(getComponent().getValue(), var::act) );
-        return this.withValue((Val<Integer>) var);
+        return this.withValue( (Val<Integer>) var );
+    }
+
+    /**
+     * Sets the major tick spacing of the slider.
+     * For more information see {@link JSlider#setMajorTickSpacing(int)}.
+     *
+     * @param spacing The major tick spacing of the slider.
+     * @return This very instance, which enables builder-style method chaining.
+     */
+    public final UIForSlider<S> withMajorTickSpacing( int spacing ) {
+        getComponent().setMajorTickSpacing( spacing );
+        return this;
+    }
+
+    /**
+     * Sets the minor tick spacing of the slider.
+     * For more information see {@link JSlider#setMinorTickSpacing(int)}.
+     *
+     * @param spacing The minor tick spacing of the slider.
+     * @return This very instance, which enables builder-style method chaining.
+     */
+    public final UIForSlider<S> withMinorTickSpacing( int spacing ) {
+        getComponent().setMinorTickSpacing( spacing );
+        return this;
+    }
+
+    /**
+     * Dynamically sets the major tick spacing of the slider.
+     * For more information see {@link JSlider#setMajorTickSpacing(int)}.
+     * @param spacing The major tick spacing of the slider.
+     * @return This very instance, which enables builder-style method chaining.
+     * @throws IllegalArgumentException if {@code spacing} is {@code null}.
+     */
+    public final UIForSlider<S> withMajorTickSpacing( Val<Integer> spacing ) {
+        NullUtil.nullArgCheck( spacing, "spacing", Val.class );
+        NullUtil.nullPropertyCheck( spacing, "spacing" );
+        _onShow( spacing, v -> getComponent().setMajorTickSpacing(v) );
+        return this;
     }
 
 }
