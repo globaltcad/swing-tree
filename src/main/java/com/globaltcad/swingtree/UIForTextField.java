a03070cfcc7e6dfecd2228835a930b045f0ed3a9
dnepp <d.nepp@globaltcad.com>
2022-12-13 17:01:17 +0100

    introduce onEnter for text fields

--- src/main/java/com/globaltcad/swingtree/UIForTextField.java
+++ src/main/java/com/globaltcad/swingtree/UIForTextField.java
@@ -1,6 +1,10 @@
 package com.globaltcad.swingtree;
 
+import com.globaltcad.swingtree.api.UIAction;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.util.function.Consumer;
 
 /**
  *  A swing tree builder node for {@link JTextField} instances.
@@ -11,4 +15,16 @@ import javax.swing.*;
 public class UIForTextField<F extends JTextField> extends UIForAbstractTextComponent<UIForTextField<F>, F>
 {
     protected UIForTextField( F component ) { super(component); }
+
+    public UIForTextField<F> onEnter( UIAction<SimpleDelegate<F, ActionEvent>> action ) {
+        NullUtil.nullArgCheck(action, "action", UIAction.class);
+        F field = getComponent();
+        _onEnter( e -> _doApp(()->action.accept(new SimpleDelegate<>( field, e, this::getSiblinghood )) ) );
+        return this;
+    }
+
+    private void _onEnter(Consumer<ActionEvent> consumer ) {
+        getComponent().addActionListener(consumer::accept);
+    }
+
 }
