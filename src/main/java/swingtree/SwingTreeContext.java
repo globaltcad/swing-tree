1adf6a1155f8b1b7e32ccfc3bd56128ed2b2fcd6
Gleethos <Gleethos@gmx.at>
2023-07-12 22:45:46 +0200

    make SwingTreeContext into a regular singleton instead of a thread local one so that all threads are aware of the current event processor

--- src/main/java/swingtree/SwingTreeContext.java
+++ src/main/java/swingtree/SwingTreeContext.java
@@ -6,35 +6,65 @@ import swingtree.threading.EventProcessor;
 import java.util.Objects;
 import java.util.Optional;
 
-public class SwingTreeContext
+/**
+ *  A {@link SwingTreeContext} is a singleton that holds global state for the SwingTree library.
+ *  This includes the {@link EventProcessor} that is used to process events, as well as the
+ *  {@link StyleSheet} that is used to style components.
+ */
+public final class SwingTreeContext
 {
-	private static final ThreadLocal<SwingTreeContext> _INSTANCES = new ThreadLocal<>();
+	private static SwingTreeContext _INSTANCES = null;
 
+	/**
+	 * Returns the singleton instance of the {@link SwingTreeContext}.
+	 * Note that this method will create the singleton if it does not exist.
+	 * @return the singleton instance of the {@link SwingTreeContext}.
+	 */
 	public static SwingTreeContext get() {
-		SwingTreeContext swingTreeContext = _INSTANCES.get();
-		if ( swingTreeContext == null ) {
-			swingTreeContext = new SwingTreeContext();
-			_INSTANCES.set(swingTreeContext);
+		// We make sure this method is thread-safe by using the double-checked locking idiom.
+		if ( _INSTANCES == null ) {
+			synchronized ( SwingTreeContext.class ) {
+				if ( _INSTANCES == null ) {
+					_INSTANCES = new SwingTreeContext();
+				}
+			}
 		}
-		return swingTreeContext;
+		return _INSTANCES;
 	}
 
 
 	private EventProcessor _eventProcessor = EventProcessor.COUPLED_STRICT;
 	private StyleSheet _styleSheet = null;
 
+	private SwingTreeContext() {}
+
+	/**
+	 * @return The currently configured {@link EventProcessor} that is used to process
+	 *         GUI and application events.
+	 */
 	public EventProcessor getEventProcessor() {
 		return _eventProcessor;
 	}
 
+	/**
+	 * Sets the {@link EventProcessor} that is used to process GUI and application events.
+	 * @param eventProcessor the {@link EventProcessor} that is used to process GUI and application events.
+	 */
 	public void setEventProcessor( EventProcessor eventProcessor ) {
 		_eventProcessor = Objects.requireNonNull(eventProcessor);
 	}
 
+	/**
+	 * @return The currently configured {@link StyleSheet} that is used to style components.
+	 */
 	public Optional<StyleSheet> getStyleSheet() {
 		return Optional.ofNullable(_styleSheet);
 	}
 
+	/**
+	 * Sets the {@link StyleSheet} that is used to style components.
+	 * @param styleSheet the {@link StyleSheet} that is used to style components.
+	 */
 	public void setStyleSheet(StyleSheet styleSheet) {
 		_styleSheet = styleSheet;
 	}
