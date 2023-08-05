8aaf32ced694fbfb8294625a55df7374851aa41b
Daniel Nepp <daniel.nepp@drei.at>
2023-08-05 14:53:55 +0200

    improve Painter documentation

--- src/main/java/swingtree/api/Painter.java
+++ src/main/java/swingtree/api/Painter.java
@@ -1,8 +1,40 @@
 package swingtree.api;
 
 
-import java.awt.*;
+import swingtree.UI;
+import swingtree.animation.Animation;
+import swingtree.animation.AnimationState;
 
+import java.awt.*;
+import java.awt.geom.RoundRectangle2D;
+import java.util.concurrent.TimeUnit;
+
+/**
+ *  Implement this functional interface to either paint <b>custom styles on components</b>
+ *  by registering painters on the style API using methods like <br>
+ *  {@link swingtree.style.ComponentStyleDelegate#painter(UI.Layer, Painter)} and <br>
+ *  {@link swingtree.style.ComponentStyleDelegate#painter(UI.Layer, String, Painter)} <br>
+ *  or <br>
+ *  for <b>defining custom animations</b> by registering painters in the animation API using the
+ *  {@link swingtree.ComponentDelegate#paint(AnimationState, Painter)} method
+ *  inside of an {@link Animation} registered through
+ *  {@link swingtree.ComponentDelegate#animateOnce(double, TimeUnit, Animation)}.
+ *  <br><br>
+ *  Note that inside the painter the {@link Graphics2D} context may not
+ *  be scaled according to the current UI scale factor (for high DPI displays). <br>
+ *  Check out the following methods for scaling you paint operations: <br>
+ *  <ul>
+ *      <li>{@link UI#scale()} - returns the current UI scale factor.</li>
+ *      <li>{@link UI#scale(Graphics2D)} - scales the given graphics context according to the current UI scale factor.</li>
+ *      <li>{@link UI#scale(double)} - scales the given value according to the current UI scale factor.</li>
+ *      <li>{@link UI#scale(float)} - scales the given value according to the current UI scale factor.</li>
+ *      <li>{@link UI#scale(int)} - scales the given value according to the current UI scale factor.</li>
+ *      <li>{@link UI#scale(Insets)} - scales the given insets according to the current UI scale factor.</li>
+ *      <li>{@link UI#scale(Dimension)} - scales the given dimension according to the current UI scale factor.</li>
+ *      <li>{@link UI#scale(Rectangle)} - scales the given rectangle according to the current UI scale factor.</li>
+ *      <li>{@link UI#scale(RoundRectangle2D)} - scales the given round rectangle according to the current UI scale factor.</li>
+ *  </ul>
+ */
 @FunctionalInterface
 public interface Painter
 {
