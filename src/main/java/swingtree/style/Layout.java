6e101b753226942dd7523a9e56f4a9f31fbd1a98
Daniel Nepp <daniel.nepp@drei.at>
2023-09-13 21:53:49 +0200

    improve documentation of the layout style API (more specifically the layout configuration API)

--- src/main/java/swingtree/style/Layout.java
+++ src/main/java/swingtree/style/Layout.java
@@ -220,15 +220,26 @@ public interface Layout
         return new ForBoxLayout( BoxLayout.X_AXIS );
     }
 
-
-    class Unspecific implements Layout
+    /**
+     *  The {@link Unspecific} layout is a layout that represents the lack
+     *  of a specific layout being set for a component.
+     *  Note that this does not represent the absence of a {@link LayoutManager}
+     *  for a component, but rather the absence of it being specified.
+     *  This means that whatever layout is currently installed for a component
+     *  will be left as is, and no other layout will be installed for the component.
+     *  <p>
+     *  Note that this is different from the {@link None} layout,
+     *  which represents the absence of a {@link LayoutManager}
+     *  for a component (i.e. it removes any existing layout from the component and sets it to {@code null}).
+     */
+    final class Unspecific implements Layout
     {
-        Unspecific(){}
+        Unspecific() {}
 
         @Override public int hashCode() { return 0; }
 
         @Override
-        public boolean equals(Object o) {
+        public boolean equals( Object o ) {
             if ( o == null ) return false;
             if ( o == this ) return true;
             return o.getClass() == getClass();
@@ -236,13 +247,24 @@ public interface Layout
 
         @Override public String toString() { return getClass().getSimpleName() + "[]"; }
 
-        @Override
-        public void installFor( JComponent component ) {
-            // Do nothing.
-        }
+        /**
+         *  Does nothing.
+         * @param component The component to install the layout for.
+         */
+        @Override public void installFor( JComponent component ) { /* Do nothing. */ }
     }
 
-    class None implements Layout
+    /**
+     *  The {@link None} layout is a layout that represents the absence
+     *  of a {@link LayoutManager} for a component.
+     *  This means that whatever layout is currently installed for a component
+     *  will be removed, and {@code null} will be set as the layout for the component.
+     *  <p>
+     *  Note that this is different from the {@link Unspecific} layout,
+     *  which does not represent the absence of a {@link LayoutManager}
+     *  for a component, but rather the absence of it being specified.
+     */
+    final class None implements Layout
     {
         None(){}
 
@@ -265,7 +287,15 @@ public interface Layout
         }
     }
 
-    class ForMigLayout implements Layout
+    /**
+     *  The {@link ForMigLayout} layout is a layout that represents
+     *  a {@link MigLayout} layout configuration for a component. <br>
+     *  Whenever this layout configuration changes,
+     *  it will create and re-install a new {@link MigLayout} onto the component
+     *  based on the new configuration,
+     *  which are the constraints, column constraints and row constraints.
+     */
+    final class ForMigLayout implements Layout
     {
         private final String _constr;
         private final String _colConstr;
@@ -273,27 +303,18 @@ public interface Layout
 
 
         ForMigLayout( String constr, String colConstr, String rowConstr ) {
-            _constr          = Objects.requireNonNull(constr);
-            _colConstr       = Objects.requireNonNull(colConstr);
-            _rowConstr       = Objects.requireNonNull(rowConstr);
+            _constr    = Objects.requireNonNull(constr);
+            _colConstr = Objects.requireNonNull(colConstr);
+            _rowConstr = Objects.requireNonNull(rowConstr);
         }
 
-        ForMigLayout withConstraint( String constr ) {
-            return new ForMigLayout( constr, _colConstr, _rowConstr );
-        }
+        ForMigLayout withConstraint( String constr ) { return new ForMigLayout( constr, _colConstr, _rowConstr ); }
 
-        ForMigLayout withRowConstraint( String rowConstr ) {
-            return new ForMigLayout( _constr, _colConstr, rowConstr );
-        }
+        ForMigLayout withRowConstraint( String rowConstr ) { return new ForMigLayout( _constr, _colConstr, rowConstr ); }
 
-        ForMigLayout withColumnConstraint( String colConstr ) {
-            return new ForMigLayout( _constr, colConstr, _rowConstr );
-        }
+        ForMigLayout withColumnConstraint( String colConstr ) { return new ForMigLayout( _constr, colConstr, _rowConstr ); }
 
-        ForMigLayout withComponentConstraint( String componentConstr ) {
-            return new ForMigLayout( _constr, _colConstr, _rowConstr );
-        }
-
+        ForMigLayout withComponentConstraint( String componentConstr ) { return new ForMigLayout( _constr, _colConstr, _rowConstr ); }
 
         @Override public int hashCode() { return Objects.hash(_constr, _rowConstr, _colConstr); }
 
@@ -329,9 +350,9 @@ public interface Layout
             if ( !_constr.isEmpty() || !_colConstr.isEmpty() || !_rowConstr.isEmpty() ) {
                 // We ensure that the parent layout has the correct layout constraints for the component:
                 LayoutManager currentLayout = component.getLayout();
-                if ( !(currentLayout instanceof MigLayout) ) {
+                if ( !( currentLayout instanceof MigLayout ) ) {
                     // We need to replace the current layout with a MigLayout:
-                    MigLayout newLayout = new MigLayout(_constr, _colConstr, _rowConstr);
+                    MigLayout newLayout = new MigLayout( _constr, _colConstr, _rowConstr );
                     component.setLayout(newLayout);
                     return;
                 }
@@ -358,19 +379,27 @@ public interface Layout
         }
     }
 
-    class ForFlowLayout implements Layout
+    /**
+     *  The {@link ForFlowLayout} layout is a layout that represents
+     *  a {@link FlowLayout} layout configuration for a component. <br>
+     *  Whenever this layout configuration changes,
+     *  it will create and re-install a new {@link FlowLayout} onto the component
+     *  based on the new configuration,
+     *  which are the alignment, horizontal gap and vertical gap.
+     */
+    final class ForFlowLayout implements Layout
     {
-        private final int align;
-        private final int hgap;
-        private final int vgap;
+        private final int _align;
+        private final int _hgap;
+        private final int _vgap;
 
         ForFlowLayout( UI.HorizontalAlignment align, int hgap, int vgap ) {
-            this.align = align.forFlowLayout();
-            this.hgap  = hgap;
-            this.vgap  = vgap;
+            _align = align.forFlowLayout();
+            _hgap  = hgap;
+            _vgap  = vgap;
         }
 
-        @Override public int hashCode() { return Objects.hash( align, hgap, vgap ); }
+        @Override public int hashCode() { return Objects.hash( _align, _hgap, _vgap  ); }
 
         @Override
         public boolean equals( Object o ) {
@@ -378,22 +407,22 @@ public interface Layout
             if ( o == this ) return true;
             if ( o.getClass() != getClass() ) return false;
             ForFlowLayout other = (ForFlowLayout) o;
-            return align == other.align && hgap == other.hgap && vgap == other.vgap;
+            return _align == other._align && _hgap == other._hgap && _vgap == other._vgap;
         }
 
         @Override
         public void installFor( JComponent component ) {
             LayoutManager currentLayout = component.getLayout();
-            if ( !(currentLayout instanceof FlowLayout) ) {
+            if ( !( currentLayout instanceof FlowLayout ) ) {
                 // We need to replace the current layout with a FlowLayout:
-                FlowLayout newLayout = new FlowLayout( align, hgap, vgap );
+                FlowLayout newLayout = new FlowLayout(_align, _hgap, _vgap);
                 component.setLayout(newLayout);
                 return;
             }
             FlowLayout flowLayout = (FlowLayout) currentLayout;
-            int alignment = align;
-            int horizontalGap = hgap;
-            int verticalGap   = vgap;
+            int alignment     = _align;
+            int horizontalGap = _hgap;
+            int verticalGap   = _vgap;
 
             boolean alignmentChanged     = alignment != flowLayout.getAlignment();
             boolean horizontalGapChanged = horizontalGap != flowLayout.getHgap();
@@ -408,25 +437,33 @@ public interface Layout
         }
     }
 
-    class BorderLayoutInstaller implements Layout
+    /**
+     *  The {@link BorderLayoutInstaller} layout is a layout that represents
+     *  a {@link BorderLayout} layout configuration for a component,
+     *  which consists of the horizontal gap and vertical gap. <br>
+     *  Whenever this layout configuration changes,
+     *  it will create and re-install a new {@link BorderLayout} onto the component
+     *  based on the new configuration.
+     */
+    final class BorderLayoutInstaller implements Layout
     {
-        private final int hgap;
-        private final int vgap;
+        private final int _hgap;
+        private final int _vgap;
 
         BorderLayoutInstaller( int hgap, int vgap ) {
-            this.hgap = hgap;
-            this.vgap = vgap;
+            _hgap = hgap;
+            _vgap = vgap;
         }
 
-        @Override public int hashCode() { return Objects.hash( hgap, vgap ); }
+        @Override public int hashCode() { return Objects.hash(_hgap, _vgap); }
 
         @Override
-        public boolean equals(Object o) {
+        public boolean equals( Object o ) {
             if ( o == null ) return false;
             if ( o == this ) return true;
             if ( o.getClass() != getClass() ) return false;
             BorderLayoutInstaller other = (BorderLayoutInstaller) o;
-            return hgap == other.hgap && vgap == other.vgap;
+            return _hgap == other._hgap && _vgap == other._vgap;
         }
 
         @Override
@@ -434,13 +471,13 @@ public interface Layout
             LayoutManager currentLayout = component.getLayout();
             if ( !(currentLayout instanceof BorderLayout) ) {
                 // We need to replace the current layout with a BorderLayout:
-                BorderLayout newLayout = new BorderLayout( hgap, vgap );
+                BorderLayout newLayout = new BorderLayout(_hgap, _vgap);
                 component.setLayout(newLayout);
                 return;
             }
             BorderLayout borderLayout = (BorderLayout) currentLayout;
-            int horizontalGap = hgap;
-            int verticalGap   = vgap;
+            int horizontalGap = _hgap;
+            int verticalGap   = _vgap;
 
             boolean horizontalGapChanged = horizontalGap != borderLayout.getHgap();
             boolean verticalGapChanged   = verticalGap   != borderLayout.getVgap();
@@ -453,21 +490,29 @@ public interface Layout
         }
     }
 
-    class GridLayoutInstaller implements Layout
+    /**
+     *  The {@link GridLayoutInstaller} layout is a layout that represents
+     *  a {@link GridLayout} layout configuration for a component,
+     *  which consists of the number of rows, number of columns, horizontal gap and vertical gap. <br>
+     *  Whenever this layout configuration changes,
+     *  it will create and re-install a new {@link GridLayout} onto the component
+     *  based on the new configuration.
+     */
+    final class GridLayoutInstaller implements Layout
     {
-        private final int rows;
-        private final int cols;
-        private final int hgap;
-        private final int vgap;
+        private final int _rows;
+        private final int _cols;
+        private final int _hgap;
+        private final int _vgap;
 
         GridLayoutInstaller( int rows, int cols, int hgap, int vgap ) {
-            this.rows = rows;
-            this.cols = cols;
-            this.hgap = hgap;
-            this.vgap = vgap;
+            _rows = rows;
+            _cols = cols;
+            _hgap = hgap;
+            _vgap = vgap;
         }
 
-        @Override public int hashCode() { return Objects.hash( rows, cols, hgap, vgap ); }
+        @Override public int hashCode() { return Objects.hash(_rows, _cols, _hgap, _vgap); }
 
         @Override
         public boolean equals(Object o) {
@@ -475,7 +520,7 @@ public interface Layout
             if ( o == this ) return true;
             if ( o.getClass() != getClass() ) return false;
             GridLayoutInstaller other = (GridLayoutInstaller) o;
-            return rows == other.rows && cols == other.cols && hgap == other.hgap && vgap == other.vgap;
+            return _rows == other._rows && _cols == other._cols && _hgap == other._hgap && _vgap == other._vgap;
         }
 
         @Override
@@ -483,15 +528,15 @@ public interface Layout
             LayoutManager currentLayout = component.getLayout();
             if ( !(currentLayout instanceof GridLayout) ) {
                 // We need to replace the current layout with a GridLayout:
-                GridLayout newLayout = new GridLayout( rows, cols, hgap, vgap );
+                GridLayout newLayout = new GridLayout(_rows, _cols, _hgap, _vgap);
                 component.setLayout(newLayout);
                 return;
             }
             GridLayout gridLayout = (GridLayout) currentLayout;
-            int rows = this.rows;
-            int cols = this.cols;
-            int horizontalGap = hgap;
-            int verticalGap   = vgap;
+            int rows          = _rows;
+            int cols          = _cols;
+            int horizontalGap = _hgap;
+            int verticalGap   = _vgap;
 
             boolean rowsChanged = rows != gridLayout.getRows();
             boolean colsChanged = cols != gridLayout.getColumns();
@@ -508,34 +553,44 @@ public interface Layout
         }
     }
 
-    class ForBoxLayout implements Layout
+    /**
+     *  The {@link ForBoxLayout} layout is a layout that represents
+     *  a {@link BoxLayout} layout configuration for a component,
+     *  which consists of the axis. <br>
+     *  The axis determines whether the layout will be a horizontal or vertical
+     *  {@link BoxLayout}. <br>
+     *  Whenever this layout configuration object changes,
+     *  it will create and re-install a new {@link BoxLayout} onto the component
+     *  based on the new configuration.
+     */
+    final class ForBoxLayout implements Layout
     {
-        private final int axis;
+        private final int _axis;
 
-        ForBoxLayout( int axis ) { this.axis = axis; }
+        ForBoxLayout( int axis ) { _axis = axis; }
 
-        @Override public int hashCode() { return Objects.hash( axis ); }
+        @Override public int hashCode() { return Objects.hash(_axis); }
 
         @Override
-        public boolean equals(Object o) {
+        public boolean equals( Object o ) {
             if ( o == null ) return false;
             if ( o == this ) return true;
             if ( o.getClass() != getClass() ) return false;
             ForBoxLayout other = (ForBoxLayout) o;
-            return axis == other.axis;
+            return _axis == other._axis;
         }
 
         @Override
         public void installFor( JComponent component ) {
             LayoutManager currentLayout = component.getLayout();
-            if ( !(currentLayout instanceof BoxLayout) ) {
+            if ( !( currentLayout instanceof BoxLayout ) ) {
                 // We need to replace the current layout with a BoxLayout:
-                BoxLayout newLayout = new BoxLayout( component, axis );
+                BoxLayout newLayout = new BoxLayout( component, _axis);
                 component.setLayout(newLayout);
                 return;
             }
             BoxLayout boxLayout = (BoxLayout) currentLayout;
-            int axis = this.axis;
+            int axis = _axis;
 
             boolean axisChanged = axis != boxLayout.getAxis();
 
