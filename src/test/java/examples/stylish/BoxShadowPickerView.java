b5acd19d25f27798c2004f69ca8d27a0a5b46105
Daniel Nepp <daniel.nepp@drei.at>
2024-01-03 21:01:44 +0100

    simplify BoxShadowPickerView example code

--- src/test/java/examples/stylish/BoxShadowPickerView.java
+++ src/test/java/examples/stylish/BoxShadowPickerView.java
@@ -198,85 +198,11 @@ public class BoxShadowPickerView extends UI.Panel
     }
 
     private static Optional<Color> parseColor(String text) {
-        // 1. Try to parse it as a hex color.
-        // 2. Try to parse it as a color name.
-        // 3. Try to parse it as a color integer.
-        // Let's go:
         try {
-            return Optional.of(Color.decode(text));
+            return Optional.ofNullable(color(text));
         } catch (NumberFormatException e) {
-            // ignore
+            return Optional.empty();
         }
-        try {
-            return Optional.of(Color.getColor(text));
-        } catch (Exception e) {
-            // ignore
-        }
-        try {
-            return Optional.of(new Color(Integer.parseInt(text)));
-        } catch (NumberFormatException e) {
-            // ignore
-        }
-        text = text.toLowerCase().trim();
-        // Let's try to parse it as a color name.
-        if ( text.equals("black") ) return Optional.of(Color.BLACK);
-        if ( text.equals("blue") )  return Optional.of(Color.BLUE);
-        if ( text.equals("cyan") )  return Optional.of(Color.CYAN);
-        if ( text.equals("darkgray") ) return Optional.of(Color.DARK_GRAY);
-        if ( text.equals("gray") ) return Optional.of(Color.GRAY);
-        if ( text.equals("green") ) return Optional.of(Color.GREEN);
-        if ( text.equals("lightgray") ) return Optional.of(Color.LIGHT_GRAY);
-        if ( text.equals("magenta") ) return Optional.of(Color.MAGENTA);
-        if ( text.equals("orange") ) return Optional.of(Color.ORANGE);
-        if ( text.equals("pink") ) return Optional.of(Color.PINK);
-        if ( text.equals("red") ) return Optional.of(Color.RED);
-        if ( text.equals("white") ) return Optional.of(Color.WHITE);
-        if ( text.equals("yellow") ) return Optional.of(Color.YELLOW);
-
-        // Lets try to split it into 4 numbers: (we allow spaces and commas)
-        String[] parts = text.split("[,\\s]+");
-        if ( parts.length == 4 ) {
-            try {
-                int r = Integer.parseInt(parts[0].trim());
-                int g = Integer.parseInt(parts[1].trim());
-                int b = Integer.parseInt(parts[2].trim());
-                int a = Integer.parseInt(parts[3].trim());
-                return Optional.of(new Color(r, g, b, a));
-            } catch (NumberFormatException e) {
-                // ignore
-            }
-            // maybe float?
-            try {
-                float r = Float.parseFloat(parts[0].trim());
-                float g = Float.parseFloat(parts[1].trim());
-                float b = Float.parseFloat(parts[2].trim());
-                float a = Float.parseFloat(parts[3].trim());
-                return Optional.of(new Color(r, g, b, a));
-            } catch (NumberFormatException e) {
-                // ignore
-            }
-        }
-        if ( parts.length == 3 ) {
-            try {
-                int r = Integer.parseInt(parts[0].trim());
-                int g = Integer.parseInt(parts[1].trim());
-                int b = Integer.parseInt(parts[2].trim());
-                return Optional.of(new Color(r, g, b));
-            } catch (NumberFormatException e) {
-                // ignore
-            }
-            // maybe float?
-            try {
-                float r = Float.parseFloat(parts[0].trim());
-                float g = Float.parseFloat(parts[1].trim());
-                float b = Float.parseFloat(parts[2].trim());
-                return Optional.of(new Color(r, g, b));
-            } catch (NumberFormatException e) {
-                // ignore
-            }
-        }
-
-        return Optional.empty();
     }
 
     public static void main( String... args ) { UI.show(f ->new BoxShadowPickerView(new BoxShadowPickerViewModel())); }
