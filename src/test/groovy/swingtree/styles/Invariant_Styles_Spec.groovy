87db1bf22a074161e6f880b560f93bdc5959ac14
Daniel Nepp <daniel.nepp@drei.at>
2024-02-07 13:37:43 +0100

    introduce work in progress unit test for radial gradients (invariant ones)

--- src/test/groovy/swingtree/styles/Invariant_Styles_Spec.groovy
+++ src/test/groovy/swingtree/styles/Invariant_Styles_Spec.groovy
@@ -186,4 +186,98 @@ class Invariant_Styles_Spec extends Specification
             Utility.similarityBetween(image1, image2) > 99.9
     }
 
+    def 'A diagonally aligned radial gradient with an offset focus point can be invariant in certain cases.'()
+    {
+        reportInfo """
+            This test demonstrates how various pairs of different styles 
+            describing radial gradients produce identical images when rendered into a BufferedImage.
+            Their invariant emerges from the order of the colors and the direction of the gradients
+            transitioning from one color to the other being reversed.
+        """
+        given : 'We declare to variables for storing pairs of identical widgets with different styles.'
+            var ui1
+            var ui2
+        when : 'We create two widgets with different styles.'
+            ui1 = UI.box()
+                    .withStyle( it -> it
+                        .size(280, 138)
+                        .borderRadius(20)
+                        .margin(10)
+                        .gradient( gradConf -> gradConf
+                            .colors(Color.BLACK, Color.CYAN, Color.GREEN, Color.MAGENTA)
+                            .boundary(UI.ComponentBoundary.CENTER_TO_CONTENT)
+                            .transition(UI.Transition.TOP_LEFT_TO_BOTTOM_RIGHT)
+                            .type(UI.GradientType.RADIAL)
+                            .focus(26,16)
+                        )
+                    )
+        and :
+            ui2 = UI.box()
+                    .withStyle( it -> it
+                        .size(280, 138)
+                        .borderRadius(20)
+                        .margin(10)
+                        .gradient( gradConf -> gradConf
+                            .colors(Color.BLACK, Color.CYAN, Color.GREEN, Color.MAGENTA)
+                            .boundary(UI.ComponentBoundary.CENTER_TO_CONTENT)
+                            .transition(UI.Transition.BOTTOM_RIGHT_TO_TOP_LEFT)
+                            .type(UI.GradientType.RADIAL)
+                            .focus(26,16)
+                        )
+                    )
+
+        and : 'We render the two widgets into a BufferedImage.'
+            var image1 = Utility.renderSingleComponent(ui1.get(JBox))
+            var image2 = Utility.renderSingleComponent(ui2.get(JBox))
+
+        then : 'They both have the expected size:'
+            image1.width == 280
+            image1.height == 138
+            image2.width == 280
+            image2.height == 138
+        and : 'The images are identical even though they have different styles:'
+            Utility.similarityBetween(image1, image2) > 99.9
+
+        when : 'We create another pair of widgets with opposite gradient transitions.'
+            ui1 = UI.box()
+                    .withStyle( it -> it
+                        .size(280, 138)
+                        .borderRadius(20)
+                        .margin(10)
+                        .gradient( gradConf -> gradConf
+                            .colors(Color.PINK, Color.GREEN, Color.BLUE)
+                            .boundary(UI.ComponentBoundary.CENTER_TO_CONTENT)
+                            .transition(UI.Transition.BOTTOM_LEFT_TO_TOP_RIGHT)
+                            .type(UI.GradientType.RADIAL)
+                            .focus(16,16)
+                        )
+                    )
+        and :
+            ui2 = UI.box()
+                    .withStyle( it -> it
+                        .size(280, 138)
+                        .borderRadius(20)
+                        .margin(10)
+                        .gradient( gradConf -> gradConf
+                            .colors(Color.PINK, Color.GREEN, Color.BLUE)
+                            .boundary(UI.ComponentBoundary.CENTER_TO_CONTENT)
+                            .transition(UI.Transition.TOP_RIGHT_TO_BOTTOM_LEFT)
+                            .type(UI.GradientType.RADIAL)
+                            .focus(16,16)
+                        )
+                    )
+
+        and : 'We render the two widgets into a BufferedImage.'
+            image1 = Utility.renderSingleComponent(ui1.get(JBox))
+            image2 = Utility.renderSingleComponent(ui2.get(JBox))
+
+        then :  'They both have the expected size:'
+            image1.width == 280
+            image1.height == 138
+            image2.width == 280
+            image2.height == 138
+        and : 'The images are identical even though they have different styles:'
+            Utility.similarityBetween(image1, image2) > 99.9
+    }
+
 }
