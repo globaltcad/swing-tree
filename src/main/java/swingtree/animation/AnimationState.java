aef4decc93355065983cda07a0d6f7d0778c2958
Gleethos <Gleethos@gmx.at>
2024-04-26 21:59:28 +0200

    introduce Progress interface and slice method for slicing an animation

--- src/main/java/swingtree/animation/AnimationState.java
+++ src/main/java/swingtree/animation/AnimationState.java
@@ -11,7 +11,7 @@ import java.util.concurrent.TimeUnit;
  * Use the numbers exposed by the methods of this value based class to define how
  * your animation should progress over time.
  */
-public final class AnimationState
+public final class AnimationState implements Progress
 {
     private final static Logger log = org.slf4j.LoggerFactory.getLogger(AnimationState.class);
 
@@ -82,244 +82,54 @@ public final class AnimationState
      * @return The animation progress in terms of a number between 0 and 1,
      *         where 0.5 means the animation is halfway through, and 1 means the animation completed.
      */
-    public double progress() { return progress; }
-
-    /**
-     *  The animation progress in the form of a value linearly growing from {@code start} to {@code end}
-     *  based on the equation {@code start + (end - start) * progress()}.
-     *  At the beginning of the animation, the value is {@code start}, at the end of the animation, the value is {@code end}.
-     *
-     * @param start The start value of the animation.
-     * @param end The end value of the animation.
-     * @return The animation progress in terms of a number between {@code start} and {@code end}.
-     */
-    public double progress( double start, double end ) { return start + (end - start) * progress(); }
-
-    /**
-     *  The animation progress in the form of a value linearly growing from 1 to 0
-     *  based on the equation {@code 1 - progress()}.
-     *  At the beginning of the animation, the value is 1, at the end of the animation, the value is 0.
-     *
-     * @return The animation progress in terms of a number between 0 and 1, where 0.5 means the animation is halfway through.
-     */
-    public double regress() { return 1 - progress(); }
-
-    /**
-     *  The animation regression in the form of a value linearly growing from {@code start} to {@code end}.
-     *  This method is equivalent to {@code progress(end, start)}.
-     *
-     * @param end The end value of the animation.
-     * @param start The start value of the animation.
-     * @return The animation progress in terms of a number between {@code start} and {@code end}.
-     */
-    public double regress( double end, double start ) { return start + (end - start) * regress(); }
-
-    /**
-     *  A sine wave oscillating between 0 and 1 and back to 0 once per iteration.
-     *  At the beginning of the animation, the value is 0, at the end of the animation, the value is 0 again,
-     *  and when the animation is halfway through, the value is 1.
-     *
-     * @return The animation progress in terms of a number between 0 and 1, where 1 means the animation is halfway through.
-     */
-    public double pulse() { return Math.sin(Math.PI * progress()); }
-
-    /**
-     *  A sine wave oscillating between {@code start} and {@code peak} and back to {@code start} once per iteration.
-     *  At the beginning of the animation, the value is {@code start}, at the end of the animation,
-     *  the value is {@code start} again,
-     *  and when the animation is halfway through, the value is {@code peak}.
-     *
-     *  @param start The start value of the sine wave.
-     *  @param peak The peak value of the sine wave.
-     *  @return The animation progress in terms of a number between {@code start} and {@code end}, where {@code end} means the animation is halfway through.
-     */
-    public double pulse( double start, double peak ) { return start + (peak - start) * pulse(); }
-
-    /**
-     *   The animation progress in the form of quickly growing sine wave front going from 0 to 1
-     *   based on the equation {@code sin(PI * progress() / 2)}.
-     *   Just like the value returned by {@link #progress()}
-     *   the {@link #jumpIn()} value starts at 0 and ends at 1,
-     *   however the crucial difference is that the {@link #jumpIn()} value
-     *   grows according to a sine wave, which makes certain animations look more natural. <br>
-     *   The returned value will grow quickly at the beginning and slowly at the end, hence the name.
-     *
-     * @return The animation progress in the form of peaking sine wave growing from 0 to 1.
-     */
-    public double jumpIn() { return Math.sin(Math.PI * progress() / 2); }
-
-    /**
-     *   The animation progress in the form of peaking sine wave growing from {@code start} to {@code end}
-     *   based on the equation {@code start + (end - start) * jumpIn()}.
-     *   Just like the value returned by {@link #progress()}
-     *   the {@link #jumpIn()} value starts at {@code start} and ends at {@code end},
-     *   however the crucial difference is that the {@link #jumpIn()} value
-     *   grows according to a sine wave, which makes certain animations look more natural. <br>
-     *   The returned value will grow quickly at the beginning and slowly at the end, hence the name.
-     *
-     * @param start The start value of the animation.
-     * @param end The end value of the animation.
-     * @return The animation progress in the form of peaking sine wave growing from {@code start} to {@code end}.
-     */
-    public double jumpIn( double start, double end ) { return start + (end - start) * jumpIn(); }
-
-    /**
-     *   The animation progress in the form of peaking sine wave growing from 1 to 0
-     *   based on the equation {@code sin(PI * (1 - progress()) / 2)}.
-     *   Just like the value returned by {@link #progress()}
-     *   the {@link #jumpOut()} value starts at 1 and ends at 0,
-     *   however the crucial difference is that the {@link #fadeOut()} value
-     *   grows according to a sine wave, which makes certain animations look more natural.
-     *
-     * @return The animation progress in the form of peaking sine wave growing from 1 to 0.
-     */
-    public double jumpOut() { return Math.sin(Math.PI * (1 - progress()) / 2); }
-
-    /**
-     *   The animation progress in the form of a initially quickly changing sine wave
-     *   going from {@code start} to {@code end}
-     *   based on the equation {@code end + (start - end) * jumpOut()}.
-     *   Just like the value returned by {@link #progress(double, double)}
-     *   the {@link #jumpOut(double, double)} value starts at {@code start} and ends at {@code end},
-     *   however the crucial difference is that the {@link #jumpOut(double, double)} value
-     *   changes according to a sine wave, which makes certain animations look more natural.
-     *
-     * @param end The end value of the animation,
-     * @param start The start value of the animation.
-     * @return The animation progress in the form of peaking sine wave growing from {@code start} to {@code end}.
-     */
-    public double jumpOut( double end, double start ) { return end + (start - end) * jumpOut(); }
-
-    /**
-     *   The animation progress in the form of peaking sine wave growing from 0 to 1
-     *   based on the equation
-     *   {@code 0.5 * (1 + Math.sin( Math.PI * (progress() - 0.5) ) )}
-     *   Just like the value returned by {@link #progress()}
-     *   the {@link #fadeIn()} value starts at 0 and ends at 1,
-     *   however the crucial difference is that the {@link #fadeIn()} value
-     *   grows according to a sine wave, which makes certain animations look more natural. <br>
-     *   The difference between this method and {@link #jumpIn()} is that the returned
-     *   value grows slower at the beginning, where {@link #jumpIn()} grows faster initially.
-     *
-     * @return The animation progress in the form of peaking sine wave growing from 0 to 1.
-     */
-    public double fadeIn() {
-        return 0.5 * (1 + Math.sin( Math.PI * (progress() - 0.5) ));
+    @Override
+    public double progress() {
+        return progress;
     }
 
     /**
-     *   The animation progress in the form of peaking sine wave growing from {@code start} to {@code end}
-     *   based on the equation {@code start + (end - start) * fadeIn()}.
-     *   Just like the value returned by {@link #progress()}
-     *   the {@link #fadeIn()} value starts at {@code start} and ends at {@code end},
-     *   however the crucial difference is that the {@link #fadeIn()} value
-     *   grows according to a sine wave, which makes certain animations look more natural. <br>
-     *   The difference between this method and {@link #jumpIn()} is that the returned
-     *   value grows slower at the beginning, where {@link #jumpIn()} grows faster initially.
+     *  Slices the progress value of this animation state into a sub-{@link Progress} of the animation
+     *  which starts with a value of {@code 0.0} when the animation reaches the progress value {@code from}
+     *  and ends with a value of {@code 1.0} when the animation reaches the progress value {@code to}.
+     *  If the {@code from} and {@code to} values are invalid, this method will correct them.
      *
-     * @param start The start value of the animation.
-     * @param end The end value of the animation.
-     * @return The animation progress in the form of a wave growing from {@code start} to {@code end}.
-     */
-    public double fadeIn( double start, double end ) { return start + (end - start) * fadeIn(); }
-
-    /**
-     *   The animation progress in the form of peaking sine wave growing from 1 to 0
-     *   based on the equation {@code 1 - fadeIn()}.
-     *   Just like the value returned by {@link #progress()}
-     *   the {@link #fadeOut()} value starts at 1 and ends at 0,
-     *   however the crucial difference is that the {@link #fadeOut()} value
-     *   grows according to a sine wave, which makes certain animations look more natural. <br>
-     *   The difference between this method and {@link #jumpOut()} is that the returned
-     *   value grows slower at the beginning, where {@link #jumpOut()} grows faster initially.
+     * @param from The progress value at which the sub-progress should start.
+     *             This value must be between 0 and 1, otherwise it will be adjusted
+     *             and a warning will be logged.
+     * @param to The progress value at which the sub-progress should end.
+     *           This value must be between 0 and 1, otherwise it will be adjusted
+     *           and a warning will be logged.
      *
-     * @return The animation progress in the form of wave growing from 1 to 0.
+     * @return A {@link Progress} object that represents the sub-progress of the animation.
+     *         This sub-progress will start with a value of {@code 0.0} when the animation reaches
+     *         the progress value {@code from} and will end with a value of {@code 1.0} when the animation
+     *         reaches the progress value {@code to}.
      */
-    public double fadeOut() { return 1 - fadeIn(); }
-
-    /**
-     *   The animation progress in the form of a sine wave going from {@code start} to {@code end}
-     *   based on the equation {@code end + (start - end) * fadeOut()}.
-     *   Just like the value returned by {@link #progress(double, double)}
-     *   the {@link #fadeOut(double, double)} value starts at {@code start} and ends at {@code end},
-     *   however the crucial difference is that the {@link #fadeOut(double, double)} value
-     *   grows according to a sine wave, which makes certain animations look more natural. <br>
-     *   The difference between this method and {@link #jumpOut()} is that the returned
-     *   value grows slower at the beginning, whereas {@link #jumpOut()} grows faster initially.
-     *
-     * @param end The end value of the animation,
-     * @param start The start value of the animation,
-     * @return The animation progress in the form a sine wave going from {@code start} to {@code end}.
-     */
-    public double fadeOut( double end, double start ) { return end + (start - end) * fadeOut(); }
-
-    /**
-     *  Defines the animation progress in terms of a number oscillating between 0, 1 and 0 once per iteration,
-     *  meaning that when the animation starts, the value is 0, when it is halfway through, the value is 1,
-     *  and when it is finished, the value is 0 again.
-     *  <p>
-     *  This is especially useful for animations that are supposed to be repeated
-     *  or whose start and end values are the same (e.g. a fade-in and fade-out animation).
-     *
-     *  @return The animation progress in terms of a number between 0 and 1,
-     *          where 1 means the animation is halfway through and 0 means the animation started or finished.
-     */
-    public double cycle() {
-        // progress() is guaranteed to be between 0 and 1, where 1 means the animation completed.
-        return 1 - Math.abs(2 * progress() - 1);
-        // The result is guaranteed to be between 0 and 1, where 1 means the animation is
-        // halfway through and 0 means the animation started or finished.
-    }
-
-    /**
-     *  Defines the animation progress in terms of a number oscillating between 0, 1 and 0 once per iteration,
-     *  meaning that when the animation starts, the value is 0, when it is halfway through, the value is 1,
-     *  and when it is finished, the value is 0 again.
-     *  <p>
-     *  This is especially useful for animations that are supposed to be repeated
-     *  or whose start and end values are the same (e.g. a fade-in and fade-out animation).
-     *  <p>
-     *  This method is similar to {@link #cycle()}, but it allows to offset the animation progress.
-     *  This is useful for animations that are supposed to be repeated and whose start and end values are different
-     *  (e.g. a fade-in and fade-out animation).
-     *
-     *  @param offset The offset of the animation progress which may be any number.
-     *                The modulo operator is used to offset the animation progress
-     *                in a way that it is always between 0 and 1.
-     *  @return The animation progress in terms of a number between 0 and 1,
-     *          where 1 means the animation is halfway through and 0 means the animation started or finished.
-     */
-    public double cyclePlus( double offset ) {
-        double progress = (progress() + offset) % 1;
-        if ( progress < 0 ) progress += 1;
-        return 1 - Math.abs(2 * progress - 1);
-    }
-
-    /**
-     *  Defines the animation progress in terms of a number oscillating between 0, 1 and 0 once per iteration,
-     *  meaning that when the animation starts, the value is 0, when it is halfway through, the value is 1,
-     *  and when it is finished, the value is 0 again.
-     *  <p>
-     *  This is especially useful for animations that are supposed to be repeated
-     *  or whose start and end values are the same (e.g. a fade-in and fade-out animation).
-     *  <p>
-     *  This method is similar to the of {@link #cyclePlus(double)} but
-     *  with the offset being subtracted instead of added.
-     *  The returned values is similar to the one returned by {@link #cycle()},
-     *  with the simple difference to offset the animation progress.
-     *  This is useful for animations that are supposed to be repeated and whose start and end values are different
-     *  (e.g. a fade-in and fade-out animation).
-     *
-     *  @param offset The offset of the animation progress which may be any number.
-     *                The modulo operator is used to offset the animation progress
-     *                in a way that it is always between 0 and 1.
-     *  @return The animation progress in terms of a number between 0 and 1,
-     *          where 1 means the animation is halfway through and 0 means the animation started or finished.
-     */
-    public double cycleMinus( double offset ) {
-        double progress = ( progress() - offset ) % 1;
-        if ( progress < 0 ) progress += 1;
-        return 1 - Math.abs(2 * progress - 1);
+    public Progress slice( double from, double to ) {
+        if ( from == 0 && to == 1 ) {
+            return this;
+        }
+        if ( from == to ) {
+            log.warn("Invalid slice from '"+from+"' to '"+to+"'", new Throwable());
+        }
+        if ( from < 0 || from > 1 || to < 0 || to > 1 ) {
+            log.warn("Invalid slice from '"+from+"' to '"+to+"'", new Throwable());
+            from = Math.min(1, Math.max(0, from));
+            to   = Math.min(1, Math.max(0, to));
+        }
+        if ( from > to ) {
+            log.warn("Invalid slice from '"+from+"' to '"+to+"'", new Throwable());
+            double tmp = from;
+            from = to;
+            to = tmp;
+        }
+        double subProgress = Math.max(from, Math.min(to, progress));
+        return new Progress() {
+            @Override
+            public double progress() {
+                return subProgress;
+            }
+        };
     }
 
     /**
