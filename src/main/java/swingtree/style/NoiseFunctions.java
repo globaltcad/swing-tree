72dffc941337df03beafebbb41fca5cc21efcfa8
Daniel Nepp <32813334+Gleethos@users.noreply.github.com>
2024-08-23 04:58:51 +0200

    152 small api improvements (#153)
    * improve documentation of LifeTime class
    
    * improve table builder API by making it immutable and better documented
    
    * add two new noise types
    
    * improve scaling of new noise types
    
    * break visible symmetry in voronoi based functions

--- src/main/java/swingtree/style/NoiseFunctions.java
+++ src/main/java/swingtree/style/NoiseFunctions.java
@@ -308,8 +308,8 @@ public final class NoiseFunctions
     }
 
     public static float voronoiBasedCellTissue(float xIn, float yIn ) {
-        float scale = 1f/32;
-        return _coordinateToWorleyDistanceValue(xIn*scale, yIn*scale);
+        float scale = 32f;
+        return _coordinateToWorleyDistanceValue(xIn/scale, yIn/scale);
     }
 
     private static float _coordinateToWorleyDistanceValue(float xIn, float yIn ) {
@@ -320,31 +320,31 @@ public final class NoiseFunctions
         final int minY2 = (int) Math.floor(yIn)     ;
         final int minY3 = (int) Math.floor(yIn) + 1 ;
         final double centerX = minX2 + _fastPseudoRandomDoubleFrom(minX2, minY2);
-        final double centerY = minY2 + _fastPseudoRandomDoubleFrom(minY2, minX2);
+        final double centerY = minY2 + _fastPseudoRandomDoubleFrom(minY2, -minX2);
         final double distanceCenter = _distanceBetween(centerX, centerY, xIn, yIn);
         final double leftX = minX1 + _fastPseudoRandomDoubleFrom(minX1, minY2);
-        final double leftY = minY2 + _fastPseudoRandomDoubleFrom(minY2, minX1);
+        final double leftY = minY2 + _fastPseudoRandomDoubleFrom(minY2, -minX1);
         final double distanceLeft = _distanceBetween(leftX, leftY, xIn, yIn);
         final double rightX = minX3 + _fastPseudoRandomDoubleFrom(minX3, minY2);
-        final double rightY = minY2 + _fastPseudoRandomDoubleFrom(minY2, minX3);
+        final double rightY = minY2 + _fastPseudoRandomDoubleFrom(minY2, -minX3);
         final double distanceRight = _distanceBetween(rightX, rightY, xIn, yIn);
         final double topX = minX2 + _fastPseudoRandomDoubleFrom(minX2, minY1);
-        final double topY = minY1 + _fastPseudoRandomDoubleFrom(minY1, minX2);
+        final double topY = minY1 + _fastPseudoRandomDoubleFrom(minY1, -minX2);
         final double distanceTop = _distanceBetween(topX, topY, xIn, yIn);
         final double bottomX = minX2 + _fastPseudoRandomDoubleFrom(minX2, minY3);
-        final double bottomY = minY3 + _fastPseudoRandomDoubleFrom(minY3, minX2);
+        final double bottomY = minY3 + _fastPseudoRandomDoubleFrom(minY3, -minX2);
         final double distanceBottom = _distanceBetween(bottomX, bottomY, xIn, yIn);
         final double topLeftX = minX1 + _fastPseudoRandomDoubleFrom(minX1, minY1);
-        final double topLeftY = minY1 + _fastPseudoRandomDoubleFrom(minY1, minX1);
+        final double topLeftY = minY1 + _fastPseudoRandomDoubleFrom(minY1, -minX1);
         final double distanceTopLeft = _distanceBetween(topLeftX, topLeftY, xIn, yIn);
         final double topRightX = minX3 + _fastPseudoRandomDoubleFrom(minX3, minY1);
-        final double topRightY = minY1 + _fastPseudoRandomDoubleFrom(minY1, minX3);
+        final double topRightY = minY1 + _fastPseudoRandomDoubleFrom(minY1, -minX3);
         final double distanceTopRight = _distanceBetween(topRightX, topRightY, xIn, yIn);
         final double bottomLeftX = minX1 + _fastPseudoRandomDoubleFrom(minX1, minY3);
-        final double bottomLeftY = minY3 + _fastPseudoRandomDoubleFrom(minY3, minX1);
+        final double bottomLeftY = minY3 + _fastPseudoRandomDoubleFrom(minY3, -minX1);
         final double distanceBottomLeft = _distanceBetween(bottomLeftX, bottomLeftY, xIn, yIn);
         final double bottomRightX = minX3 + _fastPseudoRandomDoubleFrom(minX3, minY3);
-        final double bottomRightY = minY3 + _fastPseudoRandomDoubleFrom(minY3, minX3);
+        final double bottomRightY = minY3 + _fastPseudoRandomDoubleFrom(minY3, -minX3);
         final double distanceBottomRight = _distanceBetween(bottomRightX, bottomRightY, xIn, yIn);
         double min = 1;
         min = Math.min(min, distanceCenter);
@@ -358,6 +358,292 @@ public final class NoiseFunctions
         min = Math.min(min, distanceBottomRight);
         return (float) (1 - min);
     }
+
+    public static float voronoiBasedCellMosaic(float xIn, float yIn ) {
+        float scale = 32f;
+        return _coordinateToRandomValueFromClosestWorleyCell(xIn/scale, yIn/scale);
+    }
+
+    private static float _coordinateToRandomValueFromClosestWorleyCell( float xIn, float yIn ) {
+        final int minX1 = (int) Math.floor(xIn) - 1 ;
+        final int minX2 = (int) Math.floor(xIn)     ;
+        final int minX3 = (int) Math.floor(xIn) + 1 ;
+        final int minY1 = (int) Math.floor(yIn) - 1 ;
+        final int minY2 = (int) Math.floor(yIn)     ;
+        final int minY3 = (int) Math.floor(yIn) + 1 ;
+
+        double minX = Double.POSITIVE_INFINITY;
+        double minY = Double.POSITIVE_INFINITY;
+        double minDistance = Double.POSITIVE_INFINITY;
+        final double centerX = minX2 + _fastPseudoRandomDoubleFrom(minX2, minY2);
+        final double centerY = minY2 + _fastPseudoRandomDoubleFrom(minY2, -minX2);
+        final double distanceCenter = _distanceBetween(centerX, centerY, xIn, yIn);
+        if (distanceCenter < minDistance) {
+            minDistance = distanceCenter;
+            minX = centerX;
+            minY = centerY;
+        }
+        final double leftX = minX1 + _fastPseudoRandomDoubleFrom(minX1, minY2);
+        final double leftY = minY2 + _fastPseudoRandomDoubleFrom(minY2, -minX1);
+        final double distanceLeft = _distanceBetween(leftX, leftY, xIn, yIn);
+        if (distanceLeft < minDistance) {
+            minDistance = distanceLeft;
+            minX = leftX;
+            minY = leftY;
+        }
+        final double rightX = minX3 + _fastPseudoRandomDoubleFrom(minX3, minY2);
+        final double rightY = minY2 + _fastPseudoRandomDoubleFrom(minY2, -minX3);
+        final double distanceRight = _distanceBetween(rightX, rightY, xIn, yIn);
+        if (distanceRight < minDistance) {
+            minDistance = distanceRight;
+            minX = rightX;
+            minY = rightY;
+        }
+        final double topX = minX2 + _fastPseudoRandomDoubleFrom(minX2, minY1);
+        final double topY = minY1 + _fastPseudoRandomDoubleFrom(minY1, -minX2);
+        final double distanceTop = _distanceBetween(topX, topY, xIn, yIn);
+        if (distanceTop < minDistance) {
+            minDistance = distanceTop;
+            minX = topX;
+            minY = topY;
+        }
+        final double bottomX = minX2 + _fastPseudoRandomDoubleFrom(minX2, minY3);
+        final double bottomY = minY3 + _fastPseudoRandomDoubleFrom(minY3, -minX2);
+        final double distanceBottom = _distanceBetween(bottomX, bottomY, xIn, yIn);
+        if (distanceBottom < minDistance) {
+            minDistance = distanceBottom;
+            minX = bottomX;
+            minY = bottomY;
+        }
+        final double topLeftX = minX1 + _fastPseudoRandomDoubleFrom(minX1, minY1);
+        final double topLeftY = minY1 + _fastPseudoRandomDoubleFrom(minY1, -minX1);
+        final double distanceTopLeft = _distanceBetween(topLeftX, topLeftY, xIn, yIn);
+        if (distanceTopLeft < minDistance) {
+            minDistance = distanceTopLeft;
+            minX = topLeftX;
+            minY = topLeftY;
+        }
+        final double topRightX = minX3 + _fastPseudoRandomDoubleFrom(minX3, minY1);
+        final double topRightY = minY1 + _fastPseudoRandomDoubleFrom(minY1, -minX3);
+        final double distanceTopRight = _distanceBetween(topRightX, topRightY, xIn, yIn);
+        if (distanceTopRight < minDistance) {
+            minDistance = distanceTopRight;
+            minX = topRightX;
+            minY = topRightY;
+        }
+        final double bottomLeftX = minX1 + _fastPseudoRandomDoubleFrom(minX1, minY3);
+        final double bottomLeftY = minY3 + _fastPseudoRandomDoubleFrom(minY3, -minX1);
+        final double distanceBottomLeft = _distanceBetween(bottomLeftX, bottomLeftY, xIn, yIn);
+        if (distanceBottomLeft < minDistance) {
+            minDistance = distanceBottomLeft;
+            minX = bottomLeftX;
+            minY = bottomLeftY;
+        }
+        final double bottomRightX = minX3 + _fastPseudoRandomDoubleFrom(minX3, minY3);
+        final double bottomRightY = minY3 + _fastPseudoRandomDoubleFrom(minY3, -minX3);
+        final double distanceBottomRight = _distanceBetween(bottomRightX, bottomRightY, xIn, yIn);
+        if (distanceBottomRight < minDistance) {
+            minDistance = distanceBottomRight;
+            minX = bottomRightX;
+            minY = bottomRightY;
+        }
+        return (float) _fastPseudoRandomDoubleFrom((float) minX, (float) minY);
+    }
+
+    public static float voronoiBasedPolygonCell(float xIn, float yIn ) {
+        float scale = 32f;
+        return _coordinateToClosestWorleyCellEdge(xIn/scale, yIn/scale);
+    }
+
+    private static float _coordinateToClosestWorleyCellEdge( float xIn, float yIn ) {
+        final int minX1 = (int) Math.floor(xIn) - 1 ;
+        final int minX2 = (int) Math.floor(xIn)     ;
+        final int minX3 = (int) Math.floor(xIn) + 1 ;
+        final int minY1 = (int) Math.floor(yIn) - 1 ;
+        final int minY2 = (int) Math.floor(yIn)     ;
+        final int minY3 = (int) Math.floor(yIn) + 1 ;
+
+        double closestX = Double.POSITIVE_INFINITY;
+        double closestY = Double.POSITIVE_INFINITY;
+        double secondClosestX = Double.POSITIVE_INFINITY;
+        double secondClosestY = Double.POSITIVE_INFINITY;
+        double minDistance1 = Double.POSITIVE_INFINITY;
+        double minDistance2 = Double.POSITIVE_INFINITY;
+
+        final double centerX = minX2 + _fastPseudoRandomDoubleFrom(minX2, minY2);
+        final double centerY = minY2 + _fastPseudoRandomDoubleFrom(minY2, -minX2);
+        final double distanceCenter = _distanceBetween(centerX, centerY, xIn, yIn);
+        if (distanceCenter < minDistance1 || distanceCenter < minDistance2) {
+            if (distanceCenter < minDistance1) {
+                minDistance2 = minDistance1;
+                minDistance1 = distanceCenter;
+                secondClosestX = closestX;
+                secondClosestY = closestY;
+                closestX = centerX;
+                closestY = centerY;
+            } else {
+                minDistance2 = distanceCenter;
+                secondClosestX = centerX;
+                secondClosestY = centerY;
+            }
+        }
+        final double leftX = minX1 + _fastPseudoRandomDoubleFrom(minX1, minY2);
+        final double leftY = minY2 + _fastPseudoRandomDoubleFrom(minY2, -minX1);
+        final double distanceLeft = _distanceBetween(leftX, leftY, xIn, yIn);
+        if (distanceLeft < minDistance1 || distanceLeft < minDistance2) {
+            if (distanceLeft < minDistance1) {
+                minDistance2 = minDistance1;
+                minDistance1 = distanceLeft;
+                secondClosestX = closestX;
+                secondClosestY = closestY;
+                closestX = leftX;
+                closestY = leftY;
+            } else {
+                minDistance2 = distanceLeft;
+                secondClosestX = leftX;
+                secondClosestY = leftY;
+            }
+        }
+        final double rightX = minX3 + _fastPseudoRandomDoubleFrom(minX3, minY2);
+        final double rightY = minY2 + _fastPseudoRandomDoubleFrom(minY2, -minX3);
+        final double distanceRight = _distanceBetween(rightX, rightY, xIn, yIn);
+        if (distanceRight < minDistance1 || distanceRight < minDistance2) {
+            if (distanceRight < minDistance1) {
+                minDistance2 = minDistance1;
+                minDistance1 = distanceRight;
+                secondClosestX = closestX;
+                secondClosestY = closestY;
+                closestX = rightX;
+                closestY = rightY;
+            } else {
+                minDistance2 = distanceRight;
+                secondClosestX = rightX;
+                secondClosestY = rightY;
+            }
+        }
+        final double topX = minX2 + _fastPseudoRandomDoubleFrom(minX2, minY1);
+        final double topY = minY1 + _fastPseudoRandomDoubleFrom(minY1, -minX2);
+        final double distanceTop = _distanceBetween(topX, topY, xIn, yIn);
+        if (distanceTop < minDistance1 || distanceTop < minDistance2) {
+            if (distanceTop < minDistance1) {
+                minDistance2 = minDistance1;
+                minDistance1 = distanceTop;
+                secondClosestX = closestX;
+                secondClosestY = closestY;
+                closestX = topX;
+                closestY = topY;
+            } else {
+                minDistance2 = distanceTop;
+                secondClosestX = topX;
+                secondClosestY = topY;
+            }
+        }
+        final double bottomX = minX2 + _fastPseudoRandomDoubleFrom(minX2, minY3);
+        final double bottomY = minY3 + _fastPseudoRandomDoubleFrom(minY3, -minX2);
+        final double distanceBottom = _distanceBetween(bottomX, bottomY, xIn, yIn);
+        if (distanceBottom < minDistance1 || distanceBottom < minDistance2) {
+            if (distanceBottom < minDistance1) {
+                minDistance2 = minDistance1;
+                minDistance1 = distanceBottom;
+                secondClosestX = closestX;
+                secondClosestY = closestY;
+                closestX = bottomX;
+                closestY = bottomY;
+            } else {
+                minDistance2 = distanceBottom;
+                secondClosestX = bottomX;
+                secondClosestY = bottomY;
+            }
+        }
+        final double topLeftX = minX1 + _fastPseudoRandomDoubleFrom(minX1, minY1);
+        final double topLeftY = minY1 + _fastPseudoRandomDoubleFrom(minY1, -minX1);
+        final double distanceTopLeft = _distanceBetween(topLeftX, topLeftY, xIn, yIn);
+        if (distanceTopLeft < minDistance1 || distanceTopLeft < minDistance2) {
+            if (distanceTopLeft < minDistance1) {
+                minDistance2 = minDistance1;
+                minDistance1 = distanceTopLeft;
+                secondClosestX = closestX;
+                secondClosestY = closestY;
+                closestX = topLeftX;
+                closestY = topLeftY;
+            } else {
+                minDistance2 = distanceTopLeft;
+                secondClosestX = topLeftX;
+                secondClosestY = topLeftY;
+            }
+        }
+        final double topRightX = minX3 + _fastPseudoRandomDoubleFrom(minX3, minY1);
+        final double topRightY = minY1 + _fastPseudoRandomDoubleFrom(minY1, -minX3);
+        final double distanceTopRight = _distanceBetween(topRightX, topRightY, xIn, yIn);
+        if (distanceTopRight < minDistance1 || distanceTopRight < minDistance2) {
+            if (distanceTopRight < minDistance1) {
+                minDistance2 = minDistance1;
+                minDistance1 = distanceTopRight;
+                secondClosestX = closestX;
+                secondClosestY = closestY;
+                closestX = topRightX;
+                closestY = topRightY;
+            } else {
+                minDistance2 = distanceTopRight;
+                secondClosestX = topRightX;
+                secondClosestY = topRightY;
+            }
+        }
+        final double bottomLeftX = minX1 + _fastPseudoRandomDoubleFrom(minX1, minY3);
+        final double bottomLeftY = minY3 + _fastPseudoRandomDoubleFrom(minY3, -minX1);
+        final double distanceBottomLeft = _distanceBetween(bottomLeftX, bottomLeftY, xIn, yIn);
+        if (distanceBottomLeft < minDistance1 || distanceBottomLeft < minDistance2) {
+            if (distanceBottomLeft < minDistance1) {
+                minDistance2 = minDistance1;
+                minDistance1 = distanceBottomLeft;
+                secondClosestX = closestX;
+                secondClosestY = closestY;
+                closestX = bottomLeftX;
+                closestY = bottomLeftY;
+            } else {
+                minDistance2 = distanceBottomLeft;
+                secondClosestX = bottomLeftX;
+                secondClosestY = bottomLeftY;
+            }
+        }
+        final double bottomRightX = minX3 + _fastPseudoRandomDoubleFrom(minX3, minY3);
+        final double bottomRightY = minY3 + _fastPseudoRandomDoubleFrom(minY3, -minX3);
+        final double distanceBottomRight = _distanceBetween(bottomRightX, bottomRightY, xIn, yIn);
+        if (distanceBottomRight < minDistance1 || distanceBottomRight < minDistance2) {
+            if (distanceBottomRight < minDistance1) {
+                secondClosestX = closestX;
+                secondClosestY = closestY;
+                closestX = bottomRightX;
+                closestY = bottomRightY;
+            } else {
+                secondClosestX = bottomRightX;
+                secondClosestY = bottomRightY;
+            }
+        }
+        /*
+            We now have a line that spans between 2 cell centers
+            which have their boundary in the middle.
+            We now project the current pixel position onto
+            that line.
+        */
+        double t = projectPointOntoLine(closestX, closestY, secondClosestX, secondClosestY, xIn, yIn);
+        double distanceBetweenTheTwo = _distanceBetween(closestX, closestY, secondClosestX, secondClosestY);
+        double frac = ((0.5-t)*Math.pow(distanceBetweenTheTwo, 2));
+        return (float) Math.max(0, Math.min(1, Math.sqrt(frac)));
+    }
+
+    public static double projectPointOntoLine(double x1, double y1, double x2, double y2, double px, double py) {
+        double dx = x2 - x1;
+        double dy = y2 - y1;
+        double lengthSquared = (dx * dx + dy * dy);
+
+        if (lengthSquared == 0) {
+            throw new IllegalArgumentException("The two points defining the line must not be the same.");
+        }
+
+        return ((px - x1) * dx + (py - y1) * dy) / lengthSquared;
+    }
+
 
     private static double _distanceBetween( double x1, double y1, double x2, double y2 ) {
         return Math.sqrt( (x1-x2)*(x1-x2) + (y1-y2)*(y1-y2) );
