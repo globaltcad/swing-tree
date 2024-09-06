0c2adf851a9089990fd20e69254eaa73851290ce
Daniel Nepp <daniel.nepp@drei.at>
2024-09-06 03:34:49 +0200

    make test more forgiving

--- src/test/groovy/swingtree/Scroll_Pane_Spec.groovy
+++ src/test/groovy/swingtree/Scroll_Pane_Spec.groovy
@@ -262,7 +262,7 @@ class Scroll_Pane_Spec extends Specification
             content1.getHeight() == scroll1.getViewport().getHeight()
         and : 'The content panels have the expected size.'
             content1.getWidth() > 910
-            210 <= content1.getHeight() && content1.getHeight() <= 230
+            200 <= content1.getHeight() && content1.getHeight() <= 230
             325 <= content2.getWidth()  && content2.getWidth()  <= 335
             225 <= content2.getHeight() && content2.getHeight() <= 245
     }
