8c81b33d9fdcd98c85cad6c05d5753e336a72a18
Mathias Schwengerer <mathias.schwengerer@gmail.com>
2024-10-22 23:53:51 +0200

    Add height example

--- src/test/java/examples/animated/example/Example.java
+++ src/test/java/examples/animated/example/Example.java
@@ -9,8 +9,13 @@ import java.awt.*;
 public class Example {
 
     public static void main(String[] args) {
-        example1();
-        example2();
+        UI.runLater(() -> {
+            example1();
+            example2();
+
+            //example3();
+            //example4();
+        });
     }
 
     private static void example1() {
@@ -54,6 +59,70 @@ public class Example {
             .add(UI.GROW,
                 UI.panel().withBackground(new Color(0xBBF4A0))
                     .add(new Foldable(isOpen, "foldable 2"))
+            );
+
+        UI.show(frame -> {
+            frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+            frame.setPreferredSize(new Dimension(400, 600));
+            return panel;
+        });
+    }
+
+    private static void example3() {
+        Var<Integer> height = Var.of(100);
+
+        JPanel panel = new JPanel();
+
+        UI.of(panel).withLayout("fill, wrap", "", "[][grow]")
+            .add("growx", UI.slider(UI.Align.HORIZONTAL, 10, 300, height))
+            .add(UI.GROW,
+                UI.tabbedPane().add(
+                        UI.tab("Tab 1").add(
+                            UI.panel().withBackground(new Color(0xF4A0A0))
+                                .add(UI.panel().withBackground(Color.lightGray)
+                                    .withPrefHeight(height)
+                                    .withPrefWidth(100)
+                                )
+                        )
+                    )
+                    .add(
+                        UI.tab("Tab 2").add(
+                            UI.panel().withBackground(new Color(0xBBF4A0))
+                                .add(UI.panel().withBackground(Color.lightGray)
+                                    .withPrefHeight(height)
+                                    .withPrefWidth(100)
+                                )
+                        )
+                    )
+            );
+
+        UI.show(frame -> {
+            frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+            frame.setPreferredSize(new Dimension(400, 600));
+            return panel;
+        });
+    }
+
+    private static void example4() {
+        Var<Integer> height = Var.of(100);
+
+        JPanel panel = new JPanel();
+
+        UI.of(panel).withLayout("fill", "", "[][grow]")
+            .add("wrap, growx, span 2", UI.slider(UI.Align.HORIZONTAL, 10, 300, height))
+            .add(UI.GROW,
+                UI.panel().withBackground(new Color(0xF4A0A0))
+                    .add(UI.panel().withBackground(Color.lightGray)
+                        .withPrefHeight(height)
+                        .withPrefWidth(100)
+                    )
+            )
+            .add(UI.GROW,
+                UI.panel().withBackground(new Color(0xBBF4A0))
+                    .add(UI.panel().withBackground(Color.lightGray)
+                        .withPrefHeight(height)
+                        .withPrefWidth(100)
+                    )
             );
 
         UI.show(frame -> {
