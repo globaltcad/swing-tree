e5aad18303674f15a9852b38174df2edc151b04f
Daniel Nepp <d.nepp@globaltcad.com>
2026-08-22 01:10:25 +0200

    remove unused java.util.List and Supplier imports from TabDelegate

--- src/main/java/swingtree/TabDelegate.java
+++ src/main/java/swingtree/TabDelegate.java
@@ -5,8 +5,6 @@ import sprouts.Action;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseEvent;
-import java.util.List;
-import java.util.function.Supplier;
 
 /**
  *  A {@link JTabbedPane} delegate providing useful context information to various {@link sprouts.Action} listeners
